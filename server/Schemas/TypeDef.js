1afdc503ecd82c2434fd9d443463f6b9812ec710
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-07-11 17:02:14 -0500

    schemas

---
+++ server/Schemas/TypeDef.js
@@ -0,0 +1,47 @@
+const { gql } = require ('apollo-server-express')
+
+const typedefs = gql `
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type User {
+        _id: ID
+        savedBooks: [Book]
+        email: String
+        username: String
+        password: String
+    }
+
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    input bookInput {
+        authors: [String]
+        description: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Query {
+        me: User 
+    }
+
+    type Mutation {
+        addUser(username: String, email: String, password: String): Auth
+        login(email: String, password: String): Auth
+        saveBook(bookData: bookInput): User
+        removeBook(bookId: ID): User
+    }
+
+
+
+`
---
+++ server/Schemas/index.js
@@ -0,0 +1,4 @@
+const typedefs = require ('./TypeDef.js')
+const resolvers = require ('./Resolvers')
+
+module.export = {typedefs, resolvers}
